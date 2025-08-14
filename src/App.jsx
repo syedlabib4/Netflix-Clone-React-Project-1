7d50114acfad7911ec61a9b1f4d307b7c869b5c9
labib <labibbukhari2003@gmail.com>
2025-08-14 16:14:03 +0500

    version 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Player from './pages/Player/Player';
+import Footer from './components/Footer/Footer';
 
 
 const App = () => {
   return (
+    
     <div>
       <Login />
+      <Home />
     </div>
   )
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
+import Logo from '../../assets/cards/Netflixlogo.png'
 
 const Navbar = () => {
   return (
-    <div>
-      
+<>
+  <div className='navbar '>
+    <div >
+      <img src={Logo} alt='Netflix Logo' className='navbar-logo' />
     </div>
+
+    <div className='navbar-links'>
+      <ul>
+        <li>Home</li>
+        <li>about</li>
+      </ul>
+    <div>
+      <h1>Hello</h1>
+    </div>
+    </div>
+    </div>
+</>
   )
 }
 
