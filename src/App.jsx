00b71787c8944f4d7f2d8c9a93eb92c34cfac8f7
labib <labibbukhari2003@gmail.com>
2025-08-14 15:01:29 +0500

    version 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Player from './pages/Player/Player';
+
 
 const App = () => {
   return (
     <div>
-      
+      <Login />
     </div>
   )
 }
---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <>
+      <div>
+        <h1 className='bg-red-200'>Hello</h1>
+      </div>   
+       </>
+  )
+}
+
+export default Login
